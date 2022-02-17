1780eb7f394d1732bc2f835600ca793d7745690d
651943559@qq.com <651943559@qq.com>
2022-02-17 22:38:25 +0800

    flink-connector-socket

---
+++ src/main/java/org/apache/flink/connector/table/SocketDynamicTableFactory.java
@@ -0,0 +1,85 @@
+package org.apache.flink.connector.table;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.connector.format.DecodingFormat;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.factories.DeserializationFormatFactory;
+import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SocketDynamicTableFactory implements DynamicTableSourceFactory {
+
+    private static final String IDENTIFIER = "socket";
+
+    // define all options statically
+    private static final ConfigOption<String> HOSTNAME = ConfigOptions.key("hostname")
+            .stringType()
+            .noDefaultValue()
+            .withDescription("主机IP");
+
+    private static final ConfigOption<Integer> PORT = ConfigOptions.key("port")
+            .intType()
+            .noDefaultValue()
+            .withDescription("The port of socket!!!");
+
+    private static final ConfigOption<Integer> BYTE_DELIMITER = ConfigOptions.key("byte-delimiter")
+            .intType()
+            .defaultValue(10)
+            .withDescription("默认换行符'\n'");
+
+    @Override
+    public DynamicTableSource createDynamicTableSource(Context context) {
+        // either implement your custom validation logic here ...
+        // or use the provided helper utility
+        final FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+
+        // discover a suitable decoding format
+        final DecodingFormat<DeserializationSchema<RowData>> decodingFormat = helper.discoverDecodingFormat(
+                DeserializationFormatFactory.class,
+                FactoryUtil.FORMAT);
+
+        // validate all options
+        helper.validate();
+
+        // get the validated options
+        final ReadableConfig options = helper.getOptions();
+        final String hostname = options.get(HOSTNAME);
+        final int port = options.get(PORT);
+        final byte byteDelimiter = (byte) (int) options.get(BYTE_DELIMITER);
+
+        // derive the produced data type (excluding computed columns) from the catalog table
+        final DataType producedDataType = context.getCatalogTable().getSchema().toPhysicalRowDataType();
+
+        // create and return dynamic table source
+        return new SocketDynamicTableSource(hostname, port, byteDelimiter, decodingFormat, producedDataType);
+    }
+
+    @Override
+    public String factoryIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(HOSTNAME);
+        options.add(PORT);
+        options.add(FactoryUtil.FORMAT);
+        return options;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(BYTE_DELIMITER);
+        return options;
+    }
+}
